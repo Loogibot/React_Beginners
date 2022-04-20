5647497753745a0923ac46fedfa7bc65fdf08285
Loogibot <94766364+Loogibot@users.noreply.github.com>
2022-04-20 13:20:27 -0400

    js_crud_todo

---
+++ JS_CRUD_ToDo/script.js
@@ -0,0 +1,52 @@
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+// creates data object
+let data = {};
+// stores input to data
+let acceptData = () => {
+    data["text"] = input.value;
+    console.log(data)
+    createPost();
+};
+
+// the first parentElement deletes the icons, the additional parentElements the entire post div
+let deletePost = (e) => {
+    e.parentElement.parentElement.remove();
+};
+
+// backticks `` are template literals, don't use ''
+// creates posts with that id
+let createPost = () => {
+    posts.innerHTML += `
+    <div>
+        <p>${data.text}</p>
+        <span class="options">
+        <i onClick="editPost(this)" class="fa fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>        
+        </span>
+    </div>    
+    `;
+    input.value = "";
+}
+
+// this event listener prevents the default behaviour, in this case the submit button send a form
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+
+    formValidation()
+});
+
+let formValidation = () => {
+    if (input.value === "") {
+        msg.innerHTML = "Post cannot be blank";
+        console.log("falure");
+    } else {
+        console.log("success");
+        msg.innerHTML = "";
+        acceptData();
+    }
+};
