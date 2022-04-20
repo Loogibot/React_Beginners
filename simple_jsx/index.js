500ff75471cfd79bd24b52946723e8750dfe07a5
Loogibot <94766364+Loogibot@users.noreply.github.com>
2022-04-20 09:13:31 -0400

    messy

---
+++ simple_jsx/index.js
@@ -0,0 +1,6 @@
+const h1 = document.createElement("h1")
+
+h1.textContent = "Hey Mom! this is vanilla js, the imperative way to program"
+h1.className = "header"
+
+document.getElementById("root").append(h1)
