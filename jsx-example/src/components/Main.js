50b23dcec65478347a4e643b306fa9ba759b08ae
Loogibot <94766364+Loogibot@users.noreply.github.com>
2022-04-29 12:57:57 -0400

    styled Main.js in components, fixed layout by replacing div with main, though still have padding...

--- jsx-example/src/components/Main.js
+++ jsx-example/src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Main() {
     return (
-        <div className="main">
+        <main className="main">
             <h1>Fun facts about React</h1>
             <ul>
                 <li>Was first released in 2013</li>
@@ -11,6 +11,6 @@ export default function Main() {
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </div>
+        </main>
     )
 }
--- jsx-example/src/components/Navbar.js
+++ jsx-example/src/components/Navbar.js
@@ -4,7 +4,7 @@ import reactSmallIcon from './images/react_small_icon.png'
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <img src={reactSmallIcon} />
+            <img src={reactSmallIcon} alt="icon"/>
             <h3>ReactFacts</h3>
             <h4>React Course - Project 1</h4>
         </nav>
