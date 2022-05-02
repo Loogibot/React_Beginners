bcf76db4e692caa0aff8536545c09415fa1e2345
Loogibot <94766364+Loogibot@users.noreply.github.com>
2022-05-02 16:10:56 -0400

    styling coming along, tho having issues positioning elements vertically like figma example

--- digital-business-card/src/App.js
+++ digital-business-card/src/App.js
@@ -5,9 +5,9 @@ import Main from './Components/Main';
 
 export default function App() {
   return (
-    <div>
+    <p className="container">
       <Header />
       <Main />
-    </div>
+    </p>
   );
 }
--- digital-business-card/src/Components/Header.js
+++ digital-business-card/src/Components/Header.js
@@ -3,9 +3,11 @@ import headShot from './Images/head-shot-luigi.png'
 
 export default function Header() {
     return (
-        <nav>
+        <nav className="header">
             <img className="headshot" src={headShot} alt="headshot"/>
             <h1 className="first-last-name">Luigi Mota</h1>
+            <h3 className="job-title">Frontend Developer</h3>
+            <h5 className="web-site">luigimota.com</h5>
 
 
         </nav>
