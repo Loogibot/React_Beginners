4d370f0bcf3a4d73a763d692a3e19a284bf7e249
Loogibot <94766364+Loogibot@users.noreply.github.com>
2022-05-03 10:10:44 -0400

    fixed layout somewhat, atleast everything is lined up vertically

--- digital-business-card/src/App.js
+++ digital-business-card/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Main from './Components/Main';
+import Contact from './Components/Contact'
 
 export default function App() {
   return (
     <p className="container">
       <Header />
+      <Contact />
       <Main />
     </p>
   );
--- digital-business-card/src/Components/Header.js
+++ digital-business-card/src/Components/Header.js
@@ -5,10 +5,6 @@ export default function Header() {
     return (
         <nav className="header">
             <img className="headshot" src={headShot} alt="headshot"/>
-            <h1 className="first-last-name">Luigi Mota</h1>
-            <h3 className="job-title">Frontend Developer</h3>
-            <h5 className="web-site">luigimota.com</h5>
-
 
         </nav>
 
