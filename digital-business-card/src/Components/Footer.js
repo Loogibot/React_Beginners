c7748eda829c2d2caac3cd4ad53e680abe7ef91e
Loogibot <94766364+Loogibot@users.noreply.github.com>
2022-05-03 11:02:43 -0400

    rounded the corners, fixed the socials (though still might want to break them up...). need to fix the spacing, add the email and linkedin buttons and the font is too bold, but its coming along

--- digital-business-card/src/Components/Footer.js
+++ digital-business-card/src/Components/Footer.js
@@ -5,9 +5,8 @@ import socials from './Images/social-media-foot.png'
 export default function Footer() {
     return (
         <footer className="footer">
-            <img src={socials} alt="social"></img>
+            <img className="socials" src={socials} alt="social"></img>
             
-
         </footer>
 
     )
