befb630946d4c42411f83b46151d6c1a78b9ad78
Loogibot <94766364+Loogibot@users.noreply.github.com>
2022-05-03 13:34:25 -0400

    added buttons and styled them, but the iconsare not good, they are pushing the text, the linkedin one is incorrect and I need some space bewteen the buttons, but for nowI'm just going to move on

--- digital-business-card/src/Components/Contact.js
+++ digital-business-card/src/Components/Contact.js
@@ -8,7 +8,10 @@ export default function Contact() {
             <h5 className="job-title">Frontend Developer</h5>
             <h5 className="job-title">Mobile Developer</h5>
             <h6 className="web-site">luigimota.com</h6>
-        </nav>
 
+            <button className="email-button"> <i class="material-icons" >mail</i> Email</button>
+            <button className="linkedin-button"> <i class="material-icons" >link</i> LinkedIn</button>
+
+        </nav>
     )
 };
--- digital-business-card/src/Components/Main.js
+++ digital-business-card/src/Components/Main.js
@@ -4,14 +4,14 @@ export default function Main() {
     return (
         <main>
             <h3>About</h3>
-            <h6>I am a frontend and mobile developer with a particular 
+            <p className="about-block">I am a frontend and mobile developer with a particular 
                 interest in making things accessible and using the latest 
                 tools. I'm constantly learning and exploring new ways
                 to implement design, software architecture and APIs to
-                the best of my ability.</h6>
+                the best of my ability.</p>
             <h3>Interests</h3>
-            <h6>Self taught musician. Visual artist and painter. Reader. 
-                Metal and Trap lover. Rock Climber. Gamer. Coffee fanatic.</h6>
+            <p className="interests-block">Self taught musician. Visual artist and painter. Reader. 
+                Metal and Trap lover. Rock Climber. Gamer. Coffee fanatic.</p>
 
         </main>
 
