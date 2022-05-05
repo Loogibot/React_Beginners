21e985c2fa35bd0fd6c0324c4077f0b5056a0ef2
Loogibot <94766364+Loogibot@users.noreply.github.com>
2022-05-05 11:29:16 -0400

    the beginning of props

--- airbnb-xclone/src/Components/Card.js
+++ airbnb-xclone/src/Components/Card.js
@@ -2,15 +2,20 @@ import React from 'react';
 import picture from '../Components/Images/image 12.png'
 import star from '../Components/Images/Star.png'
 
+const rating = "5.0"
+const count = "(6)"
+
 export default function Card() {
     return (
+
+
         <div className='card'>
                 <img src={picture} alt='pic' className='card-photo'></img>
             <div className='card-info'>
                 <div className='card-score'>
                     <img src={star} alt='star' className='card-star'></img>
-                    <span className='card-rating'>5.0 </span> 
-                    <span className='card-count'> (6) • USA</span>
+                    <span className='card-rating'>{rating}</span> 
+                    <span className='card-count'> {count} • USA</span>
                 </div>
 
                 <p className='card-detail'>Life lessons with Katie Zaferes</p>
