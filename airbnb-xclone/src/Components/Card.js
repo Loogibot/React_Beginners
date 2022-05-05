d84ac67065db9c83fe194a26a10afdcb9faad7d3
Loogibot <94766364+Loogibot@users.noreply.github.com>
2022-05-05 10:52:34 -0400

    matched card example, did not include the 'SOLD OUT' on-top text as per video

--- airbnb-xclone/src/Components/Card.js
+++ airbnb-xclone/src/Components/Card.js
@@ -5,10 +5,18 @@ import star from '../Components/Images/Star.png'
 export default function Card() {
     return (
         <div className='card'>
-            <img src={picture} alt='pic' className='card-picture'></img>
+                <img src={picture} alt='pic' className='card-photo'></img>
             <div className='card-info'>
-                <img src={star} alt='star' className='card-star'></img>
-                <h6>5.0</h6>
+                <div className='card-score'>
+                    <img src={star} alt='star' className='card-star'></img>
+                    <span className='card-rating'>5.0 </span> 
+                    <span className='card-count'> (6) • USA</span>
+                </div>
+
+                <p className='card-detail'>Life lessons with Katie Zaferes</p>
+                <div className='card-bottom-line'>
+                    <span className='card-price'>From $136</span><span className='card-person'> / person</span>
+                </div>
             </div>
         </div>
     )
